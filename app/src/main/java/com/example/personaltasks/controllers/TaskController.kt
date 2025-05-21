ae1d962a17b82d90fc76c99f896d4bbca4dd505a
antonioacampos <campos.antonio@aluno.ifsp.edu.br>
2025-05-21 11:13:23 -0300

    feat: implementing taskController

--- app/src/main/java/com/example/personaltasks/controllers/TaskController.kt
+++ app/src/main/java/com/example/personaltasks/controllers/TaskController.kt
@@ -1,9 +1,54 @@
 package com.example.personaltasks.controllers
 
+import android.content.Context
+import androidx.annotation.WorkerThread
 import com.example.personaltasks.model.Task
+import com.example.personaltasks.model.TaskDAO
+import com.example.personaltasks.model.TaskRoomDatabase
+import com.example.personaltasks.ui.MainActivity
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
-class TaskController {
-    fun removeTask(task: Task) {
-        //todo
+class TaskController(
+    private val taskDao: TaskDAO,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
+) {
+    companion object {
+        fun getInstance(mainActivity: MainActivity): TaskController {
+            val context = mainActivity.applicationContext
+            val database = TaskRoomDatabase.getDatabase(context)
+            return TaskController(database.taskDao())
+        }
+    }
+
+    @WorkerThread
+    suspend fun createTask(task: Task): Long = withContext(dispatcher) {
+        taskDao.insert(task)
+    }
+
+    @WorkerThread
+    suspend fun getTask(id: Long): Task? = withContext(dispatcher) {
+        taskDao.getById(id)
+    }
+
+    @WorkerThread
+    suspend fun getAllTasks(): List<Task> = withContext(dispatcher) {
+        taskDao.getAll()
+    }
+
+    @WorkerThread
+    suspend fun updateTask(task: Task): Int = withContext(dispatcher) {
+        taskDao.update(task)
+    }
+
+    @WorkerThread
+    suspend fun removeTask(task: Task): Int = withContext(dispatcher) {
+        taskDao.delete(task)
+    }
+
+    @WorkerThread
+    suspend fun removeAllTasks() = withContext(dispatcher) {
+        taskDao.deleteAll()
     }
 }
