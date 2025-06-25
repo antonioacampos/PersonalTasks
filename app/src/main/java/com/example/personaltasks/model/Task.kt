1dcdc2a65bd0f0502c099b4fa3d11a1b8eebedc1
antonioacampos <campos.antonio@aluno.ifsp.edu.br>
2025-06-25 19:50:51 -0300

    feat: aditional fields for task entity

--- app/src/main/java/com/example/personaltasks/model/Task.kt
+++ app/src/main/java/com/example/personaltasks/model/Task.kt
@@ -7,6 +7,16 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Parcelize @Entity
-data class Task(@PrimaryKey(autoGenerate = true) var id: Int, var title: String, var description: String, var dueDate: LocalDate, var isCompleted : Boolean = false) : Parcelable{
-
+data class Task(
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0,
+    val firebaseId: String = "",
+    val title: String = "",
+    val description: String = "",
+    val dueDate: LocalDate,
+    val isCompleted: Boolean = false,
+    val isDeleted: Boolean = false,
+    val userId: String = "",
+    val deletedAt: Long = 0L,
+    val createdAt: Long = System.currentTimeMillis()) : Parcelable {
 }
