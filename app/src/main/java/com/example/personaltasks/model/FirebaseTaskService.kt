696b28599a29803d26fe0103cc68137d94c0a319
antonioacampos <campos.antonio@aluno.ifsp.edu.br>
2025-06-25 19:54:23 -0300

    feat: initialize FirebaseAuth and FirebaseFirestore in FirebaseTaskService

--- app/src/main/java/com/example/personaltasks/model/FirebaseTaskService.kt
+++ app/src/main/java/com/example/personaltasks/model/FirebaseTaskService.kt
@@ -1,5 +1,13 @@
 package com.example.personaltasks.model
 
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+
 class FirebaseTaskService {
-    
+    private val db = FirebaseFirestore.getInstance()
+    private val auth = FirebaseAuth.getInstance()
+
+    private fun getCurrentUserId(): String {
+        return auth.currentUser?.uid ?: ""
+    }
 }
