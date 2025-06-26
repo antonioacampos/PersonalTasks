f5cbd598b1f446eebc01154e1ec01794d96f66ba
antonioacampos <campos.antonio@aluno.ifsp.edu.br>
2025-06-25 21:29:05 -0300

    feat: initialize variables for task details in TaskDetailsActivity

--- app/src/main/java/com/example/personaltasks/ui/TaskDetailsActivity.kt
+++ app/src/main/java/com/example/personaltasks/ui/TaskDetailsActivity.kt
@@ -1,5 +1,16 @@
 package com.example.personaltasks.ui
 
+import android.widget.TextView
+import android.widget.Toast
+import com.example.personaltasks.model.FirebaseTaskService
+import com.example.personaltasks.model.Task
+
 class TaskDetailsActivity : AppCompatActivity() {
-    
+    private lateinit var titleTextView: TextView
+    private lateinit var descriptionTextView: TextView
+    private lateinit var dueDateTextView: TextView
+    private lateinit var firebaseService: FirebaseTaskService
+
+    private var taskId: String? = null
+    private var isDeleted: Boolean = false
 }
